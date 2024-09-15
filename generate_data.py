a02319e6f5205f613bb1b06f2f029c9c92cde24b
cripry <cristian.preguza@gmail.com>
2024-09-15 22:48:47 +0300

    fix: Changed dataset

--- generate_data.py
+++ generate_data.py
@@ -17,35 +17,31 @@ load_dotenv()
 
 def data_generator(df: pd.DataFrame, endpoint: str, frequency: float = 1.0) -> None:
     """
-    Continuously extracts elements from a DataFrame ('Power' column)
+    Continuously extracts elements from a DataFrame (all columns)
     and sends them to a specified endpoint in real-time, with a configurable sending frequency.
 
     Args:
         df (pd.DataFrame): The DataFrame containing the data to be streamed.
         endpoint (str): The URL of the endpoint where data elements are sent.
         frequency (float, optional): The time interval (in seconds) between sending data points.
-                                       Defaults is 1 (every second).
+                                       Defaults to 1.0 (send every second).
 
     Raises:
-        ValueError: If the 'Power' column is not found in the DataFrame.
-        ValueError: If the frequency is less than or equal to zero.
+        ValueError: If the DataFrame is empty.
     """
 
-    if "Power" not in df.columns:
-        raise ValueError("The DataFrame must contain a column named 'Power'")
-
-    if frequency <= 0:
-        raise ValueError("Frequency must be a positive number greater than zero.")
+    if df.empty:
+        raise ValueError("The DataFrame must contain data.")
 
     while True:
-        for index, row in df.iterrows():
-            element = row["Power"]
-            requests.post(endpoint, data={"element": element})
+        for _, row in df.iterrows():
+            data = row.to_dict()  # Convert row to dictionary
+            requests.post(endpoint, json=data)  # Send as JSON
             time.sleep(frequency)
 
 
 if __name__ == "__main__":
-    df = pd.read_csv(r"data\Location1.csv")
+    df = pd.read_csv(r"data\intermittent-renewables-production-france.csv")
     endpoint = os.getenv("ENDPOINT_DATA_COLLECTION")
 
     data_generator(df, endpoint)
